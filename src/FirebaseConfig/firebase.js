2451dd9e39fbb37487e3f99e87b280d182a8979f
vanhuong <tovanhuong008@gmail.com>
2025-03-17 23:09:48 +0700

    untrack sensitive file

--- src/FirebaseConfig/firebase.js
+++ src/FirebaseConfig/firebase.js
@@ -5,12 +5,12 @@ import { getStorage } from "firebase/storage";
 
 // 🔑 Firebase configuration object
 const firebaseConfig = {
-  apiKey: "AIzaSyBvAXiEJPdniO3XeZP3KrdWrOCQp1Kryx8", // 🔑 API Key
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY, // 🔑 API Key
   authDomain: "real-estate-website-faaab.firebaseapp.com", // 🌐 Auth Domain
   projectId: "real-estate-website-faaab", // 📁 Project ID
   storageBucket: "real-estate-website-faaab.appspot.com", // 🗄️ Storage Bucket
-  messagingSenderId: "962859692737", // 📲 Messaging Sender ID
-  appId: "1:962859692737:web:ac0e8095ee1ee7696cda40", // 🆔 App ID
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID, // 📲 Messaging Sender ID
+  appId: process.env.REACT_APP_FIREBASE_APP_ID, // 🆔 App ID
   measurementId: "G-7S9LRTGCYR", // 📏 Measurement ID
 };
 
